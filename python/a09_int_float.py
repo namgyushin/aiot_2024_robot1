f001d84204c23fcd93e67d9b8a37af36c43a129d
namgyushin <namgyushin@naver.com>
2024-09-19 12:04:43 +0900

    .

---
+++ python/a09_int_float.py
@@ -0,0 +1,14 @@
+def main() :
+    print(10)
+    print(10+10)
+    print(10-10)
+    print(10*10)
+    print(10/10)
+    print(10//10)   #c++ 나누기 연산자
+    print(10%3)     #c++ 나머지 연산자와 같음
+    print(3**3)     #제곱 연산자
+
+
+
+if __name__ == "__main__":
+    main()
