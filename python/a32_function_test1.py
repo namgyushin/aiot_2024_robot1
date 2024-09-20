cf2faa48df4f417876d7b8692f874aae77e485c4
namgyushin <namgyushin@naver.com>
2024-09-20 15:49:36 +0900

    .

---
+++ python/a32_function_test1.py
@@ -0,0 +1,14 @@
+def print_n_times(n :int, *value):
+    print(type(value))
+    #a,b,c = value
+    for _ in range(n):
+        for ele in value:
+            print(ele , end=" ")
+        print()
+    # print("함수 작동 중")
+
+def main() :
+    print_n_times(3, "안녕하세요.", "파이썬", "반갑습니다", "!")
+
+if __name__ == "__main__":
+    main()
