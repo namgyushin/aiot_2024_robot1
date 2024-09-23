3673e29d2248b03776dd60812a213606804f1b9b
namgyushin <namgyushin@naver.com>
2024-09-23 12:57:24 +0900

    .

--- python/a33_function_test2.py
+++ python/a33_function_test2.py
@@ -1,29 +1,29 @@
-def print_n_times(a, b, c, *value, n = 2, **kwargs):
+
+def print_n_times(*value, n = 2, **kwargs):
     print(type(value))
     print(type(kwargs))
-    #a,b,c = value
-    for _ in range(n):                     선생님 깃허브 확인하기
+    # a,b,c = value
+    for _ in range(n):
         for ele in value:
             print(ele , end=" ")
         print()
     # print("함수 작동 중")
-    for key, vlaue in kwargs.items():
+    for key, value in kwargs.items():
         print(f"{key} : {value}")
 
-def abc_print(a,b = "요",c = ".", *d):
+def abc_print(a, b = "요", c = ".", *d):
     print(f"a: {a}")
     print(f"b: {b}")
     print(f"c: {c}")
-    return a+b+c, b+c+a
+    return a + b + c, b + c + a
 
-
-def main() :
-    print_n_times("안녕하세요.", "파이썬", "반갑습니다", n=3)
-    #abc_print("안녕", c ="하세")
-    abc_print("안녕", "하세", "입니다", "a", "b")
+def main():
+    print_n_times("안녕하세요.", "파이썬", "방갑습니다.", n=3)
+    # abc_print( "안녕", c ="하세")
+    abc_print( "안녕", "하세", "입니다.", "a", "b")
 
     # 가변 키워드 매개변수
-    print_n_times("안녕하세요.", "파이썬", "반갑습니다.", n=3, m=4, l=2)
+    print_n_times("안녕하세요.", "파이썬", "방갑습니다.", n=3, m = 4, l = 2)
 
     re1, re2 = abc_print("a", "b", "c")
     print(type(re1))
--- python/a43_class_method.py
+++ python/a43_class_method.py
@@ -1,72 +1,71 @@
-class Student:      #깃허브
+class Student:
     count = 0
     students = []
     def __init__(self, name, korean, math, english, science):
-        self.name = name
-        self.korean = korean
-        self.math = math
+        self.name =  name
+        self.korean =  korean
+        self.math =  math
         self.english = english
-        self.science = science
+        self.science =  science
         self.sum = self.get_sum()
         self.average = self.get_average()
         Student.count += 1
         Student.students.append(self)
 
-
     def get_sum(self):
         return self.korean + self.math + self.english + self.science
-    
+
     def get_average(self):
-        return self.get_sum()/4
-    
+        return self.sum / 4
+
     def to_string(self):
         return f"{self.name}\t{self.get_sum()}\t{self.get_average():.2f}"
-    
-    # def __repr__(self):
-    #     return f"{self.name}\t{self.get_sum()}\t{self.get_average():.2f}"
-    # def __str__(self):
-    #     return f"{self.name}\t{self.get_sum()}\t{self.get_average():.2f}"
-    # 위 2개로 36번라인 출력
+
+    def __str__(self):
+        return f"{self.name}\t{self.get_sum()}\t{self.get_average():.2f}"
 
     def __eq__(self, value):
-        return self.sum == value.sum
-        
+        return self.sum  == value.sum
+
     def __ne__(self, value):
-        return self.sum != value.sum
-    
+        return self.sum  != value.sum
+
     def __gt__(self, value):
-        return self.sum > value.sum
-    
+        return self.sum  > value.sum
+
     def __ge__(self, value):
-        return self.sum >= value.sum
-    
+        return self.sum  >= value.sum
+
     def __lt__(self, value):
         if isinstance(value, Student):
-            return self.sum < value.sum
+            return self.sum  < value.sum
         elif isinstance(value, int):
             return self.sum < value
+
     def __le__(self, value):
-        return self.sum <= value.sum
-    
+        return self.sum  <= value.sum
+
     @classmethod
     def print_students(cls):
-        print("------- 학생 목록 -------")
+        print("------- 학생목록 ------")
         print(f"총 학생수: {cls.count}")
         print("이름\t총점\t평균")
         for student in cls.students:
             print(student)
-        print("------------------------")
+        print("------- ------ ------")
 
-def main() :
+
+def main():
     Student("신남규", 99, 99, 99, 99)
     Student("이명철", 88, 88, 88, 88)
     Student("변새롬", 77, 77, 77, 77)
     Student("강민수", 66, 66, 66, 66)
     Student("정도현", 55, 55, 55, 55)
     Student("김승기", 44, 44, 44, 44)
-    
+
     Student.print_students()
 
-        
+
+
 if __name__ == "__main__":
     main()
