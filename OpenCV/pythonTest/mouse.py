74bd2cd3084ff5475d88aae63c19f47008c6773b
namgyushin <namgyushin@naver.com>
2024-10-15 12:43:44 +0900

    .

---
+++ OpenCV/pythonTest/mouse.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+
+
+def on_mouse(event, x, y, flags, param):
+    src = param['src']
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print(x,y)
+        cv2.circle(src, (x,y), 10, (255,0,0), -1)
+        cv2.imshow("img", src)
+
+
+def main():
+    folder = "/home/lmc/aiot_2024_robot1/OpenCV/cppTest/data/"
+    src = cv2.imread(folder+"lenna.bmp", cv2.IMREAD_COLOR)
+    
+    param = {'src':src}
+    cv2.namedWindow("img")
+    cv2.setMouseCallback("img", on_mouse, param)
+    
+    cv2.imshow("img", src)
+    cv2.waitKey(0)
+
+if __name__ == "__main__":
+    main()
