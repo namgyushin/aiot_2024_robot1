b4cd0cfcf90d9812f2f63874851306d0fe235c31
namgyushin <namgyushin@naver.com>
2024-10-15 09:47:03 +0900

    .

---
+++ OpenCV/cppTest/saturated.cpp
@@ -0,0 +1,12 @@
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+String folder = "/home/lmc/aiot_2024_robot1/OpenCV/cppTest/data/";
+int main()
+{
+    mat src = imread(folder + "lenna.bmp")
+    return 0;
+}
