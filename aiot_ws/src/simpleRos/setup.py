e8c42afb38a649f86ceb4931bc62b58141f4e32f
namgyushin <namgyushin@naver.com>
2024-10-17 16:51:26 +0900

    .

--- aiot_ws/src/simpleRos/setup.py
+++ aiot_ws/src/simpleRos/setup.py
@@ -15,11 +15,12 @@ setup(
     zip_safe=True,
     maintainer='lmc',
     maintainer_email='namgyushin@naver.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='simpleRos demo',
+    license='Apache 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            "hello = simpleRos.hello:main"
         ],
     },
 )
---
+++ aiot_ws/src/simpleRos/simpleRos/hello.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+
+def main() :
+    print("hello, ros2! nice to meet you!")
+
+if __name__ == "__main__":
+    main()
