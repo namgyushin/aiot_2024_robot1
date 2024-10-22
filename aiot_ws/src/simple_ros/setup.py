ec6b826e51c17e1e16e987a86061a378eaf051cc
namgyushin <namgyushin@naver.com>
2024-10-22 12:24:53 +0900

    .

--- aiot_ws/src/simple_ros/setup.py
+++ aiot_ws/src/simple_ros/setup.py
@@ -29,7 +29,10 @@ setup(
             "hello_pub = simple_ros.hello_pub:main",
             "time_pub = simple_ros.time_pub:main",
             "move_turtle = simple_ros.move_turtle:main",
-            "move_turtle_time = simple_ros.move_turtle_time:main"
+            "move_turtle_time = simple_ros.move_turtle_time:main",
+            "service_server = simple_ros.service_server:main",
+            "service_client = simple_ros.service_client:main"
+
         ],
     },
 )
