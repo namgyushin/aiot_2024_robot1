502cc3e78f84c4e38a08a372ed31200bd26fbe2d
namgyushin <namgyushin@naver.com>
2024-10-23 15:24:59 +0900

    .

--- aiot_ws/src/simple_ros/simple_ros/action_server.py
+++ aiot_ws/src/simple_ros/simple_ros/action_server.py
@@ -9,20 +9,23 @@ from user_interface.action import Fibonacci
 class Action_server(Node):
     def __init__(self):
         super().__init__("action_server")
-        self.action_server = ActionServer(self, Fibonacci, "fibonacci", self.execute_callback)
+        self.action_server = ActionServer(self,
+                                          Fibonacci,
+                                          "fibonacci",
+                                          execute_callback=self.execute_callback)
 
     def execute_callback(self, goal_handle):
-        self.get_logger().info(goal_handle.request.step)
+        self.get_logger().info(f"{goal_handle.request.step}")
         feedback_msg = Fibonacci.Feedback()
         feedback_msg.temp_seq = [0, 1]
         result = Fibonacci.Result()
-        
+
         for i in range(1, goal_handle.request.step):
             feedback_msg.temp_seq.append(
                 feedback_msg.temp_seq[i]+feedback_msg.temp_seq[i-1])
             goal_handle.publish_feedback(feedback_msg)
-        time.sleep(1)
-        
+            time.sleep(1)
+
         goal_handle.succeed()
         result.seq = feedback_msg.temp_seq
         return result
