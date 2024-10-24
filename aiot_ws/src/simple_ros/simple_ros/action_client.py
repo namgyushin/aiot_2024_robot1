5907b7d58e33b2891808fb38e8d3f1ffc1168858
namgyushin <namgyushin@naver.com>
2024-10-24 09:30:59 +0900

    .

--- aiot_ws/src/simple_ros/simple_ros/action_client.py
+++ aiot_ws/src/simple_ros/simple_ros/action_client.py
@@ -1,40 +1,50 @@
 import sys
 
 import rclpy
-from rclpy.action import ActionClient
+from action_msgs.msg import GoalStatus
+from rclpy.action import ActionClient, GoalResponse
+from rclpy.action.client import ClientGoalHandle
 from rclpy.node import Node
+from rclpy.task import Future
 from user_interface.action import Fibonacci
+from user_interface.action._fibonacci import Fibonacci_GetResult_Response
 
 
 class Action_client(Node):
     def __init__(self):
         super().__init__("action_client")
-        self.action_client = ActionClient(self, Fibonacci, "fibonacci")
+        self.action_client: ActionClient = ActionClient(self, Fibonacci, "fibonacci")
+        self.future = Future()
+        self.get_result_future = Future()
 
-    def send_goal(self, step):
-        goal_msg = Fibonacci.Goal()
+    def send_goal(self, step: str):
+        goal_msg : Fibonacci.Goal = Fibonacci.Goal()
         goal_msg.step = int(step)
         while(not self.action_client.wait_for_server(timeout_sec=1)):
             self.get_logger().info("fibonacci server is not available!!")
-        self.future = self.action_client.send_goal_async(goal_msg, feedback_callback=self.feedback_callback)
+        self.future : Future= self.action_client.send_goal_async(goal_msg, feedback_callback=self.feedback_callback)
         self.future.add_done_callback(self.goal_response_callback)
 
-    def goal_response_callback(self, future):
-        goal_handle = future.result()
+    def goal_response_callback(self, future : Future):
+        goal_handle : ClientGoalHandle = future.result()
         if not goal_handle.accepted:
             self.get_logger().info("Goal rejected!!!")
             return
         self.get_logger().info("Goal Accepted!!!")
-        self.get_result_future = goal_handle.get_result_async()
+        self.get_result_future : Future = goal_handle.get_result_async()
         self.get_result_future.add_done_callback(self.get_result_callback)
         self.get_logger().info("end response callback!!!")
 
-    def get_result_callback(self, future):
-        result = future.result().result
-        self.get_logger().info(f"result: {result.seq}")
+    def get_result_callback(self, future : Future):
+        result : Fibonacci_GetResult_Response = future.result()
+        if result.status == GoalStatus.STATUS_SUCCEEDED:
+            self.get_logger().info(f"result: {result.result.seq}")
+        if result.status == GoalStatus.STATUS_ABORTED:
+            self.get_logger().info("aborted!!")
 
     def feedback_callback(self, msg):
-        self.get_logger().info( f"Received feedback : {msg.feedback.temp_seq}")
+        feedback : Fibonacci.Feedback = msg.feedback
+        self.get_logger().info( f"Received feedback : {feedback.temp_seq}")
 
 def main(args = None):
     rclpy.init(args=args)
