775db9f90320eca5c59283378c69e48672d5e0d2
namgyushin <namgyushin@naver.com>
2024-10-28 11:41:09 +0900

    .

--- aiot_ws/src/simple_ros_cpp/src/logging_example.cpp
+++ aiot_ws/src/simple_ros_cpp/src/logging_example.cpp
@@ -20,6 +20,7 @@ public:
         {
             _one_shot_timer->cancel();
             RCLCPP_INFO(get_logger(), "Setting serverity Debug");
+
             auto ret = rcutils_logging_set_logger_level(
                 get_logger().get_name(), RCUTILS_LOG_SEVERITY_DEBUG);
             if (ret != RCUTILS_RET_OK)
@@ -41,13 +42,26 @@ private:
         auto msg = std_msgs::msg::String();
         msg.data = "Current count : " + std::to_string(_count);
         RCLCPP_INFO(get_logger(), msg.data.c_str());
+        RCLCPP_DEBUG_FUNCTION(get_logger(), &debug_function_to_evaluate, "Count divides into 12");
+        RCLCPP_DEBUG_EXPRESSION(get_logger(), (_count % 2) == 0, "Count is even !!");
         _pub->publish(msg);
+        _count++;
+        if (_count > 15)
+        {
+            RCLCPP_WARN(get_logger(), "Resetting count to 0");
+            _count = 0;
+        }
     }
 };
 
 bool is_divisor_of_twelve(size_t val, rclcpp::Logger logger)
 {
-    return true;
+    if (val == 0)
+    {
+        RCLCPP_ERROR(logger, "divisor cannot be 0");
+        return false;
+    }
+    return (12 % val) == 0;
 }
 
 int main()
