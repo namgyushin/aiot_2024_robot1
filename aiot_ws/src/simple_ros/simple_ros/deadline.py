c908d1779cb5a8d6c5c30d8118bfd97ee3d1eb20
namgyushin <namgyushin@naver.com>
2024-10-29 14:35:19 +0900

    .

--- aiot_ws/src/simple_ros/simple_ros/deadline.py
+++ aiot_ws/src/simple_ros/simple_ros/deadline.py
@@ -28,9 +28,22 @@ class Talker(Node):
         msg = String()
         msg.data = f'Talker says {self.publish_count}'
         self.publish_count += 1
+        self.get_logger().info(msg.data)
         if self.stop_at_count > 0 and self.publish_count >= self.stop_at_count:
             self.pub_timer.cancel()
         self.publisher.publish(msg)
+    
+    def pause_for(self, seconds):
+        if self.pause_timer:
+            return
+        self.pub_timer.cancel()
+        self.pause_timer = self.create_timer(seconds, self._pause_expired)
+    
+    def _pause_expired(self):
+        self.publish()
+        self.pub_timer.reset()
+        self.destroy_timer(self.pause_timer) #type: ignore
+        self.pause_timer = None
 
 class Listener(Node):
     def __init__(self, topic_name, qos_profile, event_callbacks, defer_subscribe=False):
@@ -56,7 +69,7 @@ class Listener(Node):
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('deadline', type=int, help='deadline QoS setting milliseconds')
-    parser.add_argument('--publish-for', type=int, dfault=5000, help='publish until pausing the talker')
+    parser.add_argument('--publish-for', type=int, default=5000, help='publish until pausing the talker')
     parser.add_argument('--pause-for', type=int, default=1000, help='pause the talker before beginning publish again')
     return parser.parse_args()
 
@@ -67,8 +80,25 @@ def main(args=None):
     deadline = Duration(seconds=parsed_args.deadline/1000.0)
     qos_profile = QoSProfile(depth=10, deadline=deadline)
     subscription_callbacks = SubscriptionEventCallbacks(
-        deadline = lambda event: get_logger("Listener").info(str(event))) # type : ignore
+        deadline = lambda event: get_logger("Listener").info(str(event)))
     listener = Listener(topic, qos_profile, event_callbacks=subscription_callbacks)
+
+    publisher_callbacks = PublisherEventCallbacks(
+        deadline = lambda event: get_logger("Talker").info(str(event)))
+    talker = Talker(topic, qos_profile, event_callbacks=publisher_callbacks)
+
+    publish_for_seconds = parsed_args.publish_for / 1000.0
+    pause_for_seconds = parsed_args.pause_for / 1000.0
+    pause_timer = talker.create_timer(publish_for_seconds, lambda: talker.pause_for(pause_for_seconds))
+
+    executor = SingleThreadedExecutor()
+    executor.add_node(listener)
+    executor.add_node(talker)
+    try:
+        executor.spin()
+    except KeyboardInterrupt:
+        rclpy.try_shutdown()
+
 
 if __name__ == '__main__':
     main()
