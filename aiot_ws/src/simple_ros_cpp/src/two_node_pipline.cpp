31d8af412d8078df5b09315e5973ac788a668c1a
namgyushin <namgyushin@naver.com>
2024-10-29 10:42:09 +0900

    .

--- aiot_ws/src/simple_ros_cpp/src/two_node_pipline.cpp
+++ aiot_ws/src/simple_ros_cpp/src/two_node_pipline.cpp
@@ -1,6 +1,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/int32.hpp"
 #include <chrono>
+#include <cinttypes>
 #include <cstdio>
 #include <string>
 
@@ -14,7 +15,8 @@ public:
         : Node(name, rclcpp::NodeOptions().use_intra_process_comms(true))
     {
         _pub = create_publisher<std_msgs::msg::Int32>(output, 10);
-        std::weak_ptr<std::remove_pointer<decltype(_pub.get())>::type> captured_pub = _pub;
+        // std::weak_ptr<std::remove_pointer<decltype(_pub.get())>::type> captured_pub = _pub;
+        rclcpp::Publisher<std_msgs::msg::Int32>::WeakPtr captured_pub = _pub;
         auto pub_callback = [captured_pub]()
         {
             auto pub_ptr = captured_pub.lock();
@@ -22,9 +24,9 @@ public:
             {
                 return;
             }
-            auto msg = std_msgs::msg::Int32();
             static int32_t count = 0;
-            msg.data = count++;
+            std_msgs::msg::Int32::UniquePtr msg(new std_msgs::msg::Int32());
+            msg->data = count++;
             printf("Pub msg : %d address : 0x%" PRIXPTR "\n", msg->data, reinterpret_cast<std::uintptr_t>(msg.get()));
             pub_ptr->publish(std::move(msg));
         };
@@ -36,33 +38,24 @@ private:
     rclcpp::TimerBase::SharedPtr _timer;
 };
 
-class Producer : public rclcpp::Node
+class Consumer : public rclcpp::Node
 {
 public:
-    Producer(const std::string &name, const std::string &output)
+    Consumer(const std::string &name, const std::string &output)
         : Node(name, rclcpp::NodeOptions().use_intra_process_comms(true))
     {
-        _pub = create_publisher<std_msgs::msg::Int32>(output, 10);
-        std::weak_ptr<std::remove_pointer<decltype(_pub.get())>::type> captured_pub = _pub;
-        auto pub_callback = [captured_pub]()
-        {
-            auto pub_ptr = captured_pub.lock();
-            if (!pub_ptr)
+        _sub = create_subscription<std_msgs::msg::Int32>(
+            output,
+            10,
+            [](std_msgs::msg::Int32::UniquePtr msg)
             {
-                return;
-            }
-            auto msg = std_msgs::msg::Int32();
-            static int32_t count = 0;
-            msg.data = count++;
-            printf("Pub msg : %d address : 0x%" PRIXPTR "\n", msg->data, reinterpret_cast<std::uintptr_t>(msg.get()));
-            pub_ptr->publish(std::move(msg));
-        };
-        _timer = create_wall_timer(1s, pub_callback);
+                printf("msg value %d address 0x%" PRIXPTR "\n", msg->data,
+                       reinterpret_cast<std::uintptr_t>(msg.get()));
+            });
     }
 
 private:
-    rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr _pub;
-    rclcpp::TimerBase::SharedPtr _timer;
+    rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr _sub;
 };
 
 int main(int argc, char *argv[])
