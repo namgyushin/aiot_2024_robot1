efd96e6f916d76d495bec09a623e771c2806f046
namgyushin <namgyushin@naver.com>
2024-11-06 10:48:32 +0900

    .

--- aiot_ws/src/move_turtle/launch/display.launch.py
+++ aiot_ws/src/move_turtle/launch/display.launch.py
@@ -6,19 +6,36 @@ from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
     default_rviz_config_path = PathJoinSubstitution([
-                FindPackageShare('urdf_tutorial_path'),
+                FindPackageShare('move_turtle'),
                 'rviz',
                 'urdf.rviz'])
-    gui_arg = DeclareLaunchArgument(name='gui', default_value='true', choices=['true', 'false'], description='Flag to enable joint_state_publisher_gui')
-    rviz_arg = DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path, description='Absolute path to rviz config file')
+    default_model_path = PathJoinSubstitution([
+                'urdf',
+                'myfirst.urdf'])
+    model = DeclareLaunchArgument(
+        name='model',
+        default_value=default_model_path,
+        description='urdf path')
+    gui_arg = DeclareLaunchArgument(
+        name='gui',
+        default_value='true',
+        choices=['true', 'false'],
+        description='Flag to enable joint_state_publisher_gui')
+    rviz_arg = DeclareLaunchArgument(
+        name='rvizconfig',
+        default_value=default_rviz_config_path,
+        description='Absolute path to rviz config file')
     return LaunchDescription([
+        model,
+        gui_arg,
+        rviz_arg,
         IncludeLaunchDescription(
             PathJoinSubstitution([
-                FindPackageShare('urdf_launch'),
+                FindPackageShare('move_turtle'),
                 'launch',
                 'display.launch.py']),
             launch_arguments={
-                'urdf_package': 'urdf_tutorial',
+                'urdf_package': 'move_turtle',
                 'urdf_package_path': LaunchConfiguration('model'),
                 'rviz_config': LaunchConfiguration('rvizconfig'),
                 'jsp_gui': LaunchConfiguration('gui')
--- aiot_ws/src/move_turtle/setup.py
+++ aiot_ws/src/move_turtle/setup.py
@@ -1,3 +1,6 @@
+import os
+from glob import glob
+
 from setuptools import find_packages, setup
 
 package_name = 'move_turtle'
@@ -10,6 +13,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob(os.path.join('launch', '*.launch.py'))),
+        ('share/' + package_name + '/urdf', glob(os.path.join('urdf', '*.urdf'))),
+        ('share/' + package_name + '/rviz', glob(os.path.join('rviz', '*.rviz'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
