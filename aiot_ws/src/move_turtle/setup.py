deb96d0aa388e2ac8b77a82dc462dbdbe72d7f45
namgyushin <namgyushin@naver.com>
2024-11-06 14:07:52 +0900

    .

--- aiot_ws/src/move_turtle/setup.py
+++ aiot_ws/src/move_turtle/setup.py
@@ -16,6 +16,7 @@ setup(
         ('share/' + package_name + '/launch', glob(os.path.join('launch', '*.launch.py'))),
         ('share/' + package_name + '/urdf', glob(os.path.join('urdf', '*.urdf'))),
         ('share/' + package_name + '/rviz', glob(os.path.join('rviz', '*.rviz'))),
+        ('share/' + package_name + '/meshes', glob(os.path.join('meshes', '*.*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
