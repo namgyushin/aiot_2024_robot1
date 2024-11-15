a7eb06d1708c68d5332a8e1840e6359d9d587601
namgyushin <namgyushin@naver.com>
2024-11-15 12:53:02 +0900

    .

--- aiot_ws/src/move_turtle/move_turtle/arduino_servo.py
+++ aiot_ws/src/move_turtle/move_turtle/arduino_servo.py
@@ -1,7 +1,12 @@
+import math
+
 import rclpy
 import serial
+import tf_transformations
+from geometry_msgs.msg import TransformStamped
 from rclpy.node import Node
 from std_msgs.msg import String
+from tf2_ros import Buffer, TransformBroadcaster
 
 
 class Arduino_servo(Node):
@@ -9,8 +14,24 @@ class Arduino_servo(Node):
         super().__init__("arduino_servo")
         self.create_subscription(String, "servo", self.sub_callback, 10)
         self.ser = serial.Serial('/dev/ttyACM0', 115200)
+        self.tf_broadcaster = TransformBroadcaster(self)
 
     def sub_callback(self, msg: String):
+        servo_tf = TransformStamped()
+        servo_tf.header.stamp = self.get_clock().now().to_msg()
+        servo_tf.header.frame_id = "base_link"
+        servo_tf.child_frame_id = "flag"
+        theta = int(msg.data[4:7])
+        q = tf_transformations.quaternion_from_euler(0, -theta/180*math.pi, 0)
+        servo_tf.transform.translation.x = 0.025  
+        servo_tf.transform.translation.y = -0.05
+        servo_tf.transform.translation.z = 0.14
+        servo_tf.transform.rotation.x = q[0]
+        servo_tf.transform.rotation.y = q[1]
+        servo_tf.transform.rotation.z = q[2]
+        servo_tf.transform.rotation.w = q[3]
+        self.tf_broadcaster.sendTransform(servo_tf)
+        
         byte_msg = (msg.data+'\n').encode('utf-8')
         self.ser.write(byte_msg)
         self.get_logger().info(msg.data)
--- arduino_project/servo/servo.ino
+++ arduino_project/servo/servo.ino
@@ -10,30 +10,31 @@ void setup()
 {
     myServo.attach(SERVO_PIN);
 
-    // Serial.begin(115200);
+    Serial.begin(115200);
 }
 
 void loop()
 {
-    // static String buffer;
-    // if (Serial.available() > 0)
+    static String buffer;
+    if (Serial.available() > 0)
+    {
+        buffer = Serial.readStringUntil('\n');
+        Serial.print(buffer.substring(0, 4));
+        if (buffer.substring(0, 4) == "move")
+        {
+            int pos = buffer.substring(4, 7).toInt();
+            myServo.write(pos);
+        }
+        Serial.flush();
+    }
+    // for (angle = 0; angle < 180; angle++)
     // {
-    //     buffer = Serial.readStringUntil('\n');
-    //     if (buffer.substring(0, 4) == "move")
-    //     {
-    //         int pos = buffer.substring(4, 7).toInt();
-    //         myServo.write(pos);
-    //     }
-    //     Serial.flush();
+    //     myServo.write(angle);
+    //     delay(15);
     // }
-    for (angle = 0; angle < 18; angle++)
-    {
-        myServo.write(angle);
-        delay(15);
-    }
-    for (angle = 18; angle > 0; angle--)
-    {
-        myServo.write(angle);
-        delay(15);
-    }
+    // for (angle = 180; angle > 0; angle--)
+    // {
+    //     myServo.write(angle);
+    //     delay(15);
+    // }
 }
