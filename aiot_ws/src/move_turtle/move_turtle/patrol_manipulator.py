8d38c946b4462e2c417462c756583df3bfef162a
namgyushin <namgyushin@naver.com>
2024-11-18 16:53:42 +0900

    .

--- aiot_ws/src/move_turtle/move_turtle/patrol_manipulator.py
+++ aiot_ws/src/move_turtle/move_turtle/patrol_manipulator.py
@@ -1,6 +1,8 @@
 import rclpy
 from open_manipulator_msgs.srv import SetJointPosition
+from rclpy.clock import Duration
 from rclpy.node import Node
+from rclpy.task import Future
 
 
 class Patrol_manipulator(Node):
@@ -12,23 +14,55 @@ class Patrol_manipulator(Node):
         self.request = SetJointPosition.Request()
         self.create_timer(1/60, self.update)
         self.joint_angles = self.request.joint_position.position
+        self.prev_time = self.get_clock().now()
+        self.stage = 0
 
-    def send_request(self):
+    def send_request(self, path_time=0.5):
         self.request.joint_position.joint_name = ['joint1', 'joint2', 'joint3', 'joint4']
         self.request.joint_position.position = self.joint_angles
+        self.request.path_time = path_time
         self.future = self.client.call_async(self.request)
         self.future.add_done_callback(self.done_callback)
 
-    def done_callback(self, future):
+    def done_callback(self, future : Future):
         response : SetJointPosition.Response = future.result()
-        self.get_logger().info(f"{response.success}")
-        self.get_logger().info(response.message)
+        self.get_logger().info(f"{response.is_planned}")
 
     def update(self):
-        self.joint_angles[0]
-        self.joint_angles[1]
-        self.joint_angles[2]
-        self.joint_angles[3]
+        if self.prev_time + Duration(seconds=3) > self.get_clock().now():
+            # first move
+            self.joint_angles = [0.123, 0.123, 0.123, 0.123 , 0.123]
+            if self.stage == 0:
+                self.send_request()
+                self.stage += 1
+        elif self.prev_time + Duration(seconds=5) > self.get_clock().now():
+            # second move
+            self.joint_angles = [-0.123, -0.123, -0.123, -0.123 , -0.123]
+            if self.stage == 1:
+                self.send_request()
+                self.stage += 1
+        elif self.prev_time + Duration(seconds=6) > self.get_clock().now():
+            # second move
+            self.joint_angles = [0.456 , 0.456, 0.456, 0.456 , 0.456]
+            if self.stage == 2:
+                self.send_request()
+                self.stage += 1
+        elif self.prev_time + Duration(seconds=7) > self.get_clock().now():
+            # second move
+            self.joint_angles = [-0.456, -0.456, -0.456, -0.456 , -0.456]
+            if self.stage == 3:
+                self.send_request()
+                self.stage += 1
+
+        elif self.prev_time + Duration(seconds=8) > self.get_clock().now():
+            # second move
+            self.joint_angles = [0.999, -0.999, 0.999, -0.999]
+            if self.stage == 4:
+                self.send_request()
+                self.stage += 1
+        elif self.prev_time + Duration(seconds=10) > self.get_clock().now():
+            self.prev_time = self.get_clock().now()
+            self.stage = 0
 
 def main():
     rclpy.init()
